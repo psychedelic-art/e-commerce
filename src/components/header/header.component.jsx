11385a811a704eb3f3822e4dd0cc74a97d861974
Pshichedelic <tecnologiainovamm@gmail.com>
2021-06-03 00:11:48 -0500

    header componet, header component created with styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../icons/crown.svg'
+import './header.styles.scss'
+
 const Header = () => {
- return (<div>
-     <div></div>
-     <div></div>
+ return (<div className="header-wrapper">
+     <div className="logo-wrapper">
+         <Link to="/">
+            <Logo />
+         </Link>
+     </div>
+     <div className="nav-links-wrapper">
+        <Link className="nav-link" to="/shopping">SHOP</Link>
+        <Link className="nav-link">CONTACT</Link>
+        <Link className="nav-link">SIGN IN</Link>
+     </div>
  </div>)
 }
 
